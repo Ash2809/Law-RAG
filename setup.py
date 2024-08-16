41eb0ea9a074f752a1b3389033d303d619e1c63f
Ash2809 <aashutoshkumar2809@gmail.com>
2024-08-16 19:22:06 +0530

    updated exp2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = 'Langgraph Lawyer RAG Chat-Bot',
-    version= '1.0.0',
+    version= '2.0.0',
     author= 'Aashutosh Kumar',
     author_email= 'aashutoshkumar2809@gmail.com',
     packages= find_packages(),
